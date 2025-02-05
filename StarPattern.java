c6940415058aa3850da49f7c0ead5f29b2171f18
Abdul khan <abdulkhan333c@gmail.com>
2025-02-05 15:53:05 +0530

    Add files via upload

---
+++ StarPattern.java
@@ -0,0 +1,13 @@
+
+class StarPatten{
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            for (int j = 0; j < i; j++) {
+                System.err.print("*");
+            }
+            System.err.println("");
+        }
+    }
+
+}
