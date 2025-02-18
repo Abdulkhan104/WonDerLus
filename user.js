5d5a82e165550f44806d474594cc034519618543
Abdul khan <abdulkhan333c@gmail.com>
2025-02-18 11:57:05 +0530

    Add files via upload

--- user.js
+++ user.js
@@ -1,58 +1,13 @@
-const express = require("express");
-const router = express.Router();
-const User = require("../models/user.js");
-const wrapAsync = require("../utils/wrapAsync.js");
-const passport = require("passport");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportlocalMongoose = require("passport-local-mongoose");
 
-
-router.get("/signup", (req, res) =>{
-    res.render("users/signup.ejs");
+const userSchema = new Schema({
+    email:{
+        type:String,
+        required:true
+    },
 });
 
-
-router.post(
-    "/signup",
-    wrapAsync (async(req, res)=>{
-    try {
-        let {username, email, password} = req.body;
-        const newUser = new User({email, username});
-        const registeredUser = await User.register(newUser, password);
-        console.log(registeredUser);
-        req.flash("success","Wellcome To Wanderlust");
-        res.redirect("/listings");
-    
-    }catch(e) {
-        req.flash("error",e.message);
-        res.redirect("/signup");
-      }
-
-   })
-);
-
-router.get("/login",(req, res)=>{
-    res.render("users/login.ejs");
-});
-
-router.post(
-    "/login",
-     passport.authenticate("local",{ 
-     failureRedirect: '/login',
-     failureFlash: true,
-    }),
-     async(req,res) => {
-        req.flash("success", "Welcome to Wanderlust !!");
-        res.redirect("/listings");
-     }
-);
-
- router.get("/logout",(req, res, next) => {
-    req.logout((err) => {
-        if(err){
-            return next(err);
-        }
-        req.flash("success","You are logged out!");
-        res.redirect("/listings");
-        });
- });
- 
-module.exports = router;
+userSchema.plugin(passportlocalMongoose);
+module.exports = mongoose.model('User', userSchema);
